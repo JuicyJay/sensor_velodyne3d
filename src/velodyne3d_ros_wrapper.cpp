1eb916ae1d655f7c9c43a117b7589d9d8abee9da
Jasmin Dschessica <juicy@jessica.web>
2019-12-17 21:21:31 +0100

    subscriber for velo pointcloud2, instantiated own sensormodel SensorVelodyne3D

--- src/velodyne3d_ros_wrapper.cpp
+++ src/velodyne3d_ros_wrapper.cpp
@@ -5,19 +5,30 @@
  *      Author: jasmin
  */
 
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 #include "obvision/reconstruct/space/SensorVelodyne3D.h"
+#include "obcore/math/mathbase.h"
 
+void velodyneCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+  std::cout << __PRETTY_FUNCTION__ << "callback comin" << std::endl;
+}
 
 int main(int argc, char** argv)
 {
 
   ros::init(argc, argv, "velodyne3d_node");
+  ros::NodeHandle n;
+
+  ros::Subscriber subVelodyne = n.subscribe("velodyne_points", 1000, velodyneCallback);
+
   unsigned int raysIncl = 16;
+  double inclMin        = obvious::deg2rad(-15.0);
   double inclRes        = 2.0;
   double azimRes        = 3.0;
   //SensorVelodyne3D(unsigned int raysIncl, double inclMin, double inclRes, double azimRes, double maxRange=INFINITY, double minRange=0.0, double lowReflectivityRange=INFINITY);
-  obvious::SensorVelodyne3D(raysIncl, inclRes, azimRes);
+  obvious::SensorVelodyne3D(raysIncl, inclMin, inclRes, azimRes);
   ros::spin();
 }
 
